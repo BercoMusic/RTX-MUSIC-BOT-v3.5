bd1d306e1b2432496f1aca4d919ef6c82b070c7d
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-02-13 11:31:51 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `shâdow shâznessi çok seviyor.`, 
+  name: `/help`, 
   type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
 } else {
