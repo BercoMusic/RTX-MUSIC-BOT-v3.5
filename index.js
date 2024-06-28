25a7f382aca26b5200fbdd39e885c3f9eb69e400
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-06-28 20:56:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 process.env.YTDL_NO_UPDATE = 'true';
 const config = require('./config.js');
-
 if(config.shardManager.shardStatus == true){
-
-const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
-
+  const { ShardingManager } = require('discord.js');
+  const manager = new ShardingManager('./bot.js', { token: config.TOKEN });
+  manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+  manager.spawn();
 } else {
-
-require("./bot.js")
-
+  require("./bot.js");
 }
 /*
 
