de8695fa5b5b8aea81de04851bf81d18f910a41b
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-06-28 21:25:44 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,51 +1,34 @@
-const { ApplicationCommandOptionType, EmbedBuilder } = require("discord.js");
-const { QueryType } = require("discord-player");
+const { ApplicationCommandOptionType, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { QueryType } = require('discord-player');
 
 module.exports = {
   name: "play",
-  description: "Şarkı çalar",
-  permissions: "0x0000000000000800",
+  description: "Play a song!",
   options: [{
-    name: 'name',
-    description: 'Çalmak istediğiniz şarkının adını yazın.',
+    name: 'query',
     type: ApplicationCommandOptionType.String,
-    required: true
+    description: 'The song you want to play',
+    required: true,
   }],
-  voiceChannel: true,
-  run: async (client, interaction) => {
+  async execute(interaction, client) {
+    if (!interaction.member.voice.channel) 
+      return interaction.reply({ content: "You need to be in a voice channel to play music!", ephemeral: true });
+
+    const query = interaction.options.getString("query", true);
+    
+    await interaction.deferReply();
+
     try {
-      const name = interaction.options.getString('name');
-      if (!name) return interaction.reply({ content: "Lütfen geçerli bir şarkı adı girin.", ephemeral: true });
-
-      const { channel } = interaction.member.voice;
-      if (!channel) return interaction.reply({ content: "Bir ses kanalında olmalısınız!", ephemeral: true });
-
-      const res = await client.player.search(name, {
-        requestedBy: interaction.member,
-        searchEngine: QueryType.AUTO
+      const { track } = await client.player.play(interaction.member.voice.channel, query, {
+        nodeOptions: {
+          metadata: interaction
+        }
       });
 
-      if (!res || !res.tracks.length) return interaction.reply({ content: "Sonuç bulunamadı!", ephemeral: true });
-
-      const queue = await client.player.createQueue(interaction.guild, {
-        metadata: interaction.channel
-      });
-
-      try {
-        if (!queue.connection) await queue.connect(channel);
-      } catch {
-        await client.player.deleteQueue(interaction.guildId);
-        return interaction.reply({ content: "Ses kanalına katılamıyorum!", ephemeral: true });
-      }
-
-      await interaction.reply({ content: `${res.tracks[0].title} yükleniyor... 🎧` });
-
-      res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
-
-      if (!queue.playing) await queue.play();
-    } catch (e) {
-      console.error(e);
-      interaction.reply({ content: "Bir hata oluştu: " + e, ephemeral: true });
+      return interaction.followUp(`**${track.title}** enqueued!`);
+    } catch (error) {
+      console.error(error);
+      return interaction.followUp({ content: `There was an error while executing this command: ${error.message}`, ephemeral: true });
     }
   },
 };
