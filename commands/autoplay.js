a6a106bb0df809932879729907f557be7039abb7
BercoMusic <128734731+BercoMusic@users.noreply.github.com>
2024-06-29 14:02:02 +0300

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -14,30 +14,31 @@
    ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
 */
 
+const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
 const db = require("../mongoDB");
-const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "autoplay",
-  description: "Toggle the autoplay of the queue.",
-  options: [],
+  description: "Toggle the autoplay mode.",
   permissions: "0x0000000000000800",
+  options: [],
+  voiceChannel: true,
   run: async (client, interaction) => {
     try {
-      const queue = client?.player?.getQueue(interaction?.guild?.id);
-      if (!queue || !queue?.playing) {
-        return interaction?.reply({ content: '⚠️ No music playing!!', ephemeral: true });
-      }
-      
-      queue?.toggleAutoplay();
-      
+      const queue = client.player.getQueue(interaction.guild.id);
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+
+      queue.toggleAutoplay();
+
       const embed = new EmbedBuilder()
-        .setColor('#2f58fe')
-        .setTitle('Your Music, Your Call!!')
-        .setDescription(queue?.autoplay ? '**✅ Autoplay ON**' : '**❌ Autoplay OFF**')
-        
+        .setColor(queue.autoplay ? client.config.embedColor : client.config.errorColor)
+        .setTitle(client.user.username + " - Autoplay")
+        .setDescription(`**Autoplay Mode:** \`${queue.autoplay ? 'Active' : 'Inactive'}\``)
+        .setTimestamp()
+        .setFooter({ text: 'MusicMaker ❤️' })
       
-      interaction?.reply({ embeds: [embed] });
+      return interaction.reply({ embeds: [embed] });
+      
     } catch (e) {
       console.error(e);
     }
